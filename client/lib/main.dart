8d243fd59184247c922231d117b37e2c20be9fa3
Bartłomiej Dec <bapidec@gmail.com>
2023-04-15 20:59:01 +0200

    Created client app

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,115 +1,260 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<List<Product>> fetchProducts() async {
+  final response =
+      await http.get(Uri.parse('http://localhost:8081/api/products'));
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  if (response.body != null) {
+    List<Product> products = (json.decode(response.body) as List)
+        .map((data) => Product.fromJson(data))
+        .toList();
+    return products;
+  } else {
+    throw Exception('Failed to load products');
+  }
+}
+
+class Product {
+  final int productId;
+  final String productName;
+  final double price;
+  final String? countryOfOrigin;
+  final String category;
+  final bool available;
+
+  Product({
+    required this.productId,
+    required this.productName,
+    required this.price,
+    required this.category,
+    required this.available,
+    this.countryOfOrigin,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      productId: json['product_id'],
+      productName: json['product_name'],
+      price: json['price'],
+      countryOfOrigin: json['country_of_origin'],
+      category: json['category'],
+      available: json['available'],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'REST client',
+        theme: ThemeData(
+            useMaterial3: false,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent)),
+        debugShowCheckedModeBanner: false,
+        home: const MyHomePage(
+          title: 'Home page',
+        ),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  Product? currentProduct;
+
+  void showItemDetails(Product? product) {
+    currentProduct = product;
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return appState.currentProduct == null
+        ? const ListOfProducts()
+        : ProductDetails(product: appState.currentProduct!);
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ListOfProducts extends StatefulWidget {
+  const ListOfProducts({super.key});
 
-  void _incrementCounter() {
+  @override
+  State<ListOfProducts> createState() => _ListOfProductsState();
+}
+
+class _ListOfProductsState extends State<ListOfProducts> {
+  late Future<List<Product>> _futureProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureProducts = fetchProducts();
+  }
+
+  void _reloadData() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _futureProducts = fetchProducts();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('List of products'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 10.0, 0),
+            child: IconButton(
+                onPressed: () {
+                  _reloadData();
+                },
+                icon: const Icon(Icons.refresh_rounded)),
+          )
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have smashed this button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        child: FutureBuilder<List<Product>>(
+            future: _futureProducts,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Product> products = snapshot.data!;
+                return Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: ListView(
+                    padding: const EdgeInsets.all(5.0),
+                    children: [
+                      for (Product product in products)
+                        ProductTile(product: product)
+                    ],
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            }),
+      ),
+    );
+  }
+}
+
+class ProductTile extends StatelessWidget {
+  const ProductTile({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.add_shopping_cart),
+        title: Text(product.productName),
+        onTap: () {
+          appState.showItemDetails(product);
+        },
+      ),
+    );
+  }
+}
+
+class ProductDetails extends StatelessWidget {
+  const ProductDetails({super.key, required this.product});
+  final Product product;
+  final TextStyle descriptionStyle = const TextStyle(
+    fontSize: 20,
+  );
+  final double descriptionPadding = 8.0;
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('${product.productName} details'),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 10.0, 0),
+              child: IconButton(
+                  onPressed: () {
+                    appState.showItemDetails(null);
+                  },
+                  icon: const Icon(Icons.arrow_back_ios_new_rounded)),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          padding: const EdgeInsets.all(10.0),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(descriptionPadding),
+                    child: Text(
+                      product.productName,
+                      style: const TextStyle(
+                          fontSize: 32, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(descriptionPadding),
+                    child: Text(
+                      'Price: ${product.price} \$',
+                      style: descriptionStyle,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(descriptionPadding),
+                    child: Text(
+                      'Category: ${product.category}',
+                      style: descriptionStyle,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(descriptionPadding),
+                    child: Text(
+                      'Country of origin: ${product.countryOfOrigin ?? ''}',
+                      style: descriptionStyle,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(descriptionPadding),
+                    child: Text(
+                      'Available: ${product.available ? 'yes' : 'no'}',
+                      style: descriptionStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
